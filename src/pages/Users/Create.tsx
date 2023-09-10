50d3afa5d3924131e10f4e34ef9a1b8d300eb9fb
GabrielProjec <gabriel.acesso3@gmail.com>
2023-09-10 11:45:52 -0300

    Forms and Api CEP

--- src/pages/Users/Create.tsx
+++ src/pages/Users/Create.tsx
@@ -1,7 +1,22 @@
+import { Box, Paper, Stack } from "@mui/material";
+import Form from "./components/Form";
+import PageTitle from "../../components/PageTitle";
+import Breadcrumbs from "../../components/Breadcumbs";
+
 function Create() {
   return (
     <div>
-      <h1>Pagina de Cadastro</h1>
+      <Stack direction={{ xs: "column", sm: "row" }} gap={1} mb={2}>
+        <Box sx={{ flexGrow: 1 }}>
+          <PageTitle title="Lista" />
+          <Breadcrumbs
+            path={[{ label: "Usuarios", to: "/users/" }, { label: "Novo" }]}
+          />
+        </Box>
+      </Stack>
+      <Paper>
+        <Form />
+      </Paper>
     </div>
   );
 }
